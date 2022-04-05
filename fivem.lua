d7cdc1587ceb4f269b4d0e1b2300fdab456b732a
Hendrich <99266969+H3NDR1CH@users.noreply.github.com>
2022-04-05 20:13:19 -0300

    Update fivem.lua

--- fivem.lua
+++ fivem.lua
@@ -1,6 +1,6 @@
 Citizen.CreateThread(function()
 for taxi=1, 200000 do
-TriggerServerEvent("damn_lixeiro:rewardTrash")
-Citizen.Wait(1300)
+TriggerServerEvent("damn_lixeiro:rewardTrash")  -- Here's the Job
+Citizen.Wait(1300) -- You can change the Delay (Less = More money, but more risk)
 end
  end)
